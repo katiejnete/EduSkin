e501a637f53e5bedeaa9d6181b97a27e172d6557
Kate Vong <katiejnete@gmail.com>
2024-05-20 13:56:38 -0700

    remove bind

--- app.py
+++ app.py
@@ -33,7 +33,6 @@ with app.app_context():
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     app.config["SQLALCHEMY_ECHO"] = False
     app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY", "it's a secret")
-    app.run(host='0.0.0.0', port=10000)
     connect_db(app)
 
 
